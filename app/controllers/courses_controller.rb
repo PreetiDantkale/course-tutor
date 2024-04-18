540db511bb59787c20e9c43d436b7b747d9d6ff9
Preeti Dantkale <Preeti>
2024-04-18 20:19:37 +0530

    Use nested_attributes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,7 +7,6 @@ class CoursesController < ApplicationController
   def create
     course = Course.new(course_params)
     if course.save
-      create_tutors(course)
       render json: { course: course, tutors: course.tutors }, status: :created
     else
       render json: { errors: course.errors }, status: :unprocessable_entity
@@ -17,16 +16,7 @@ class CoursesController < ApplicationController
   private
 
   def course_params
-    params.require(:course).permit(:name, :status, :code, :start_date, :end_date, :price, :max_capacity)
-  end
-
-  def tutor_params
-    params.require(:tutors).map { |t| t.permit(:name, :email, :phone, :address, :experience, :specialization, :status) }
-  end
-
-  def create_tutors(course)
-    tutor_params.each do |tutor_param|
-      course.tutors.create(tutor_param)
-    end
+    params.require(:course).permit(:name, :status, :code, :start_date, :end_date, :price, :max_capacity,
+      tutors_attributes: [:name, :email, :phone, :address, :experience, :specialization, :status, :_destroy])
   end
 end
